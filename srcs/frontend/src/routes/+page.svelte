043e05675a47498d4e5664e04713928f71e42c1b
Pometcan <pometcan@gmail.com>
2024-11-18 10:05:40 +0300

    mini fix

---
+++ srcs/frontend/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Auth from "$lib/components/Auth.svelte";
+    import Game from "$lib/components/Game.svelte";
+    let navPage = $state(Auth);
+</script>
+
+<div class="grid text-center">
+    <button class="" onclick={(navPage = Auth)}>Auth</button>
+    <button onclick={(navPage = Game)}>Game</button>
+</div>
+
+<div>
+    {@render navPage()}
+</div>
