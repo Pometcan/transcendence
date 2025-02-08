80a97cb374e3afae29de8ef5e198690167325252
Pometcan <pometcan@gmail.com>
2025-02-09 02:27:06 +0300

    fix

--- srcs/frontend/src/pages/FriendPage.js
+++ srcs/frontend/src/pages/FriendPage.js
@@ -5,8 +5,55 @@ const FriendPage = {
   layoutVisibility: true,
   render: () => {
     const pageContainer = MenuElement("FriendPage");
-    const searchInput = SearchInput("searchInput", "Search for a friend");
-    const userListDiv= new DivComponent("userListDiv", {class: ""});
+    const searchInput = SearchInput("searchInput", "Sea");
+    withEventHandlers(searchInput.elements[0].elements[2], {onClick: async () => {
+     await findUser(searchInput.elements[0].elements[0].value).then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        const row = new DivComponent(data[i].id, {class: ""});
+        row.elements = [
+          new TextComponent("username", { text: data[i].username }),
+          new ImageComponent("avatar", { src: data[i].avatar }),
+          new ButtonComponent(`addFriend${data[i].id}`, { label: "add Friend ", class: "btn btn-primary" })
+        ];
+        withEventHandlers(row.elements[0], {onClick: () => {
+          window.router.navigate(`/profile`, {id: data[i].id});
+        }});
+        withEventHandlers(row.elements[1], {onClick: () => {
+          window.router.navigate(`/profile`, {id: data[i].id});
+        }});
+        withEventHandlers(row.elements[2], {onClick: async () => {
+          const csrfToken = await getCsrfToken();
+          await fetch(`https://${window.location.host}/api/auth/sent-friendship-request/`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              "X-CSRFToken": csrfToken,
+              "Authorization": `Bearer ${getCookie('accessToken')}`
+            },
+            body: JSON.stringify({receiver: data[i].id})
+          })
+          .then((response) => {
+            console.log("addFriend Yanıt Durumu:", response.status, response.statusText);
+            if (!response.ok) {
+              const message = `HTTP error! status: ${response.status}`;
+              throw new Error(message);
+            }
+            console.log("Friend request sent");
+            window.router.navigate("/friends");
+          })
+          .catch((error) => {
+            console.error("addFriend Hatası:", error);
+          });
+        }})
+        userListDiv.elements.push(row);
+
+      userListDiv.update( {elements: userListDiv.elements} );
+      }})}});
+
+
+    const userListDiv= new DivComponent("userListDiv", {class: "", style: {overflowY: "scroll"}});
     getAllUser()
       .then((data) => {
       for (let i = 0; i < data.length; i++) {
@@ -90,5 +137,34 @@ async function getAllUser() {
     return data;
   } catch (error) {
       console.error("getAllUser Hatası:", error);
+  }
+}
+
+async function findUser(username) {
+  try {
+    const csrfToken = await getCsrfToken();
+
+    const response = await fetch(`https://${window.location.host}/api/auth/search-user?search=${username}`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+          Accapt: "application/json",
+          "Content-Type": "application/json",
+          "X-CSRFToken": csrfToken,
+          "Authorization": `Bearer ${getCookie('accessToken')}`
+      }
+    });
+
+    console.log("findUser Yanıt Durumu:", response.status, response.statusText); // Yanıt durumunu logla
+
+    if (!response.ok) {
+      const message = `HTTP error! status: ${response.status}`;
+      throw new Error(message);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+      console.error("findUser Hatası:", error);
   }
 }
